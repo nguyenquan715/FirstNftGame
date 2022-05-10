199fb4cbfcbdf27afcf59539aa842a7885b89526
QuanNA <quanna@relipasoft.com>
2022-05-10 23:35:43 +0700

    logic game

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -36,6 +36,9 @@ contract MyEpicGame is ERC721 {
 
   BigBoss public bigBoss;
 
+  event CharacterMinted(address sender, uint256 tokenId, uint32 characterIndex);
+  event AttackComplete(address sender, uint newBossHp, uint newPlayerHp);
+
   constructor(
     string[] memory characterNames,
     string[] memory characterImageURIs,
@@ -73,12 +76,14 @@ contract MyEpicGame is ERC721 {
   }
 
   function mintCharacter(uint32 _characterIndex) external {
+    require(nftHolders[msg.sender] == 0, "Error: User has character already");
     uint256 newItemId = _tokenIds.current();
     _safeMint(msg.sender, newItemId);
     Character memory characterClone = defaultCharacters[_characterIndex];
     nftAttributes[newItemId] = characterClone;
     nftHolders[msg.sender] = newItemId;
     _tokenIds.increment();
+    emit CharacterMinted(msg.sender, newItemId, _characterIndex);
     console.log("Minted NFT with tokenId %s and characterIndex %s", newItemId, _characterIndex);
   }
 
@@ -106,5 +111,48 @@ contract MyEpicGame is ERC721 {
     );
 
     return output;
+  }
+
+  function attackBoss() public {
+    uint256 characterId = nftHolders[msg.sender];
+    Character storage character = nftAttributes[characterId];
+    console.log("\nPlayer with character %s about to attack. Has %s HP and %s AD", character.name, character.hp, character.attackDamage);
+    console.log("Boss %s has %s HP and %s AD", bigBoss.name, bigBoss.hp, bigBoss.attackDamage);
+    require(character.hp > 0, "Error: character must have HP to attack boss.");
+    require(bigBoss.hp > 0, "Error: boss must have HP to attack boss.");
+    // player attack boss
+    if (character.attackDamage > bigBoss.hp) {
+      bigBoss.hp = 0;
+    } else {
+      bigBoss.hp -= character.attackDamage;
+    }
+    // boss attack player
+    if (bigBoss.attackDamage > character.hp) {
+      character.hp = 0;
+    } else {
+      character.hp -= bigBoss.attackDamage;
+    }
+
+    emit AttackComplete(msg.sender, bigBoss.hp, character.hp);
+    console.log("Player attacked boss. New boss hp: %s", bigBoss.hp);
+    console.log("Boss attacked player. New player hp: %s\n", character.hp);
+  }
+
+  function checkIfUserHasNft() public view returns (Character memory) {
+    uint256 characterId = nftHolders[msg.sender];
+    if (characterId == 0) {
+      Character memory emptyCharacter;
+      return emptyCharacter;
+    } else {
+      return nftAttributes[characterId];
+    }
+  }
+
+  function getAllDefaultCharacters() public view returns (Character[] memory) {
+    return defaultCharacters;
+  }
+
+  function getBigBoss() public view returns (BigBoss memory) {
+    return bigBoss;
   }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,38 +1,41 @@
 const main = async () => {
-  const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
-  const gameContract = await gameContractFactory.deploy(
-    ["Captain", "Thor", "Iron man"], // names
-    ["https://i.pinimg.com/564x/1e/d0/0d/1ed00d7fcf893cfe305b4903243d9070.jpg", // images
+  // character info
+  const characterNames = ["Captain", "Thor", "Iron Man", "Spider Man", "Doctor Strange", "Wanda", "Hulk", "Winter Solider", "Black Widow", "Hawkeye"];
+  const characterImages = [
+    "https://i.pinimg.com/564x/1e/d0/0d/1ed00d7fcf893cfe305b4903243d9070.jpg",
     "https://i.pinimg.com/564x/b3/03/ec/b303ec6b705423b5de0e160c4e3a0f36.jpg", 
-    "https://i.pinimg.com/564x/f9/6a/2b/f96a2b05cf23883ad4e3a82f70780465.jpg"],
-    [200, 150, 100], // HP
-    [50, 100, 100], // attack damage
-    "Thanos",
-    "https://i.pinimg.com/564x/9e/27/ba/9e27ba0ebcfc9031afa945eda4eaf719.jpg",
-    10000,
-    50
+    "https://i.pinimg.com/564x/f9/6a/2b/f96a2b05cf23883ad4e3a82f70780465.jpg",
+    "https://i.pinimg.com/564x/2e/cf/15/2ecf1550640e266185961d96439f488c.jpg",
+    "https://i.pinimg.com/564x/53/ef/9b/53ef9b5294a9f5a5db55b46a9818fd55.jpg",
+    "https://i.pinimg.com/564x/b6/a6/8b/b6a68b80666d97ccf2a2b149b01d3289.jpg",
+    "https://i.pinimg.com/564x/3b/90/f1/3b90f1f9906ee32231a42b5e458f99d6.jpg",
+    "https://i.pinimg.com/564x/75/b4/b1/75b4b16cca9e9ca8945df7b35b159fbd.jpg",
+    "https://i.pinimg.com/564x/db/26/be/db26bed4a2527c9867db86f6246d3f33.jpg",
+    "https://i.pinimg.com/564x/79/1e/fb/791efb4e6d04d5454204cae4ae96c756.jpg"
+  ];
+  const characterHps = [200, 150, 100, 100, 100, 75, 150, 75, 75, 75];
+  const characterDamages = [50, 125, 100, 75, 125, 150, 150, 50, 50, 75];
+  // boss info
+  const bossName = "Thanos";
+  const bossImage = "https://i.pinimg.com/564x/9e/27/ba/9e27ba0ebcfc9031afa945eda4eaf719.jpg";
+  const bossHp = 10000;
+  const bossDamage = 50;
+
+  // deployment
+  const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
+  const [owner, ...otherUsers] = await hre.ethers.getSigners();
+  const gameContract = await gameContractFactory.deploy(
+    characterNames,
+    characterImages,
+    characterHps,
+    characterDamages,
+    bossName,
+    bossImage,
+    bossHp,
+    bossDamage
   );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
-  
-  let txn;
-  txn = await gameContract.mintCharacter(0);
-  await txn.wait();
-  console.log("Minted NFT #1");
-
-  txn = await gameContract.mintCharacter(1);
-  await txn.wait();
-  console.log("Minted NFT #2");
-
-  txn = await gameContract.mintCharacter(2);
-  await txn.wait();
-  console.log("Minted NFT #3");
-
-  txn = await gameContract.mintCharacter(1);
-  await txn.wait();
-  console.log("Minted NFT #4");
-
-  console.log("Done deploying and minting!");
 };
 
 const runMain = async () => {
@@ -46,3 +49,4 @@ const runMain = async () => {
 };
 
 runMain();
+//0x4e0e2557394f503E3B9f7A0e84D2B6F55833C5c6
